791657bfa4679d76995ab1e16c4a2d2c3c487ab5
Chitra-3 <68221440+Chitra-3@users.noreply.github.com>
2023-07-27 13:32:04 +0530

    Add files via upload

---
+++ src/components/MyForm.js
@@ -0,0 +1,34 @@
+import Input from "@mui/material/Input"
+import Button from "@mui/material/Button"
+import { useDispatch, useSelector } from "react-redux"
+import { setUserSlice } from "../redux/slice/user"
+import { nanoid } from "@reduxjs/toolkit"
+import { CREATE_USER, UPDATE_USER_BY_ID } from "../redux/types"
+const MyForm = () => {
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const handleChange = (prop) => (event) => {
+        dispatch(setUserSlice({ ...user, [prop]: event.target.value }))
+    }
+    const handleSubmit = () => {
+        user.id === 0 ? dispatch({ type: CREATE_USER, user: { ...user, id: nanoid(8) } }) : dispatch({ type: UPDATE_USER_BY_ID, user })
+
+        dispatch(setUserSlice({
+            id: 0,
+            name: '',
+            email: '',
+            password: ''
+        }))
+    }
+    return <>
+        <>
+            <Input style={{ margin: '10px' }} margin="none" value={user.id} fullWidth disabled />
+
+            <Input style={{ margin: '10px' }} onChange={handleChange('name')} placeholder="Enter Name" value={user.name} fullWidth />
+            <Input style={{ margin: '10px' }} onChange={handleChange('email')} placeholder="Enter Email" value={user.email} fullWidth />
+            <Input style={{ margin: '10px' }} onChange={handleChange('password')} placeholder="Enter Password" value={user.password} fullWidth />
+            <Button style={{ margin: '10px' }} onClick={() => handleSubmit()} fullWidth variant="contained">Submit</Button>
+        </>
+    </>
+}
+export default MyForm
